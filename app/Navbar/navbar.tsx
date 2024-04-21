625f99fefca0ccfd4fe8221400ec73eb7ee49f00
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-04-21 14:21:24 +0600

    responsive added

--- app/Navbar/navbar.tsx
+++ app/Navbar/navbar.tsx
@@ -1,11 +1,13 @@
 import '../CSS/Home.css'
-
+import { FaFacebook } from "react-icons/fa";
+import { IoLogoInstagram } from "react-icons/io5";
+import { FaSkype } from "react-icons/fa";
 import Link from 'next/link'
 
 
 const HomePage = () => {
     return (
-        <div className="min-h-screen w-full">
+        <div className="min-h-screen w-full ">
             <div className="homeContainer flex justify-between w-[96%] m-auto pt-4">
                 <div className="sideBarContainer">
                     <div className="text-[#fff] uppercase leading-4 text-[12px]">
@@ -14,7 +16,7 @@ const HomePage = () => {
                                 <li>
                                     <Link
                                         className='active text-[#BCAB10] ' href="/">Home
-                                        <span className='hoverActive '><hr className='mt-[2px] bg-yellow-500 border-none h-[2px]'/></span> </Link>
+                                        <span className='hoverActive '><hr className='mt-[2px] bg-yellow-500 border-none h-[2px]' /></span> </Link>
                                 </li>
                                 <li> <Link href="/about">about</Link></li>
                                 <li> <Link href="/services">services</Link></li>
@@ -26,8 +28,20 @@ const HomePage = () => {
 
 
                 </div>
-                <div className="contentContainer">
-                    <h1 className=" text-red-600">gggg</h1>
+                <div className="contentContainer text-[#fff] mr-[20px]">
+                    <nav>
+                        <ul className='flex gap-4'>
+                            <li>
+                                <Link
+                                    className='active text-[#BCAB10] ' href="/"><FaFacebook size={25} />
+                                </Link>
+                            </li>
+                            <li> <Link className='active text-[#BCAB10] ' href="/about"><IoLogoInstagram size={25} /></Link></li>
+                            <li> <Link className='active text-[#BCAB10] ' href="/services"><FaSkype size={25} /></Link></li>
+
+                        </ul>
+                    </nav>
+
 
                 </div>
 
